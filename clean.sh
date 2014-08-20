c65c360d5f37da2b7e82e2f5b93d2da455a08592
Masataro Asai <guicho2.71828@gmail.com>
2014-08-20 11:52:42 +0900

    fix: verbosity handling

--- clean.sh
+++ clean.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
+echo "##### START CLEANING #############################################"
+
 pushd ${1:-$(pwd)}
 pwd
 rm -fv *.sas *.sasp *.cost *.log *.time *.plan.* downward.tmp.*
 popd
+
+echo "##### END CLEANING #############################################"
+
