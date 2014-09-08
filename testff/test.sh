dd8b8a13d23e8f97f2676d07504f90d158732383
Masataro Asai <guicho2.71828@gmail.com>
2014-09-08 12:43:08 +0900

    using common version of macroff

---
+++ testff/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+../cgroup-setup.sh
+
+## ff
+# this should finish normally (because it is easy)
+../clean.sh
+../limit.sh ../macroff-clean p01.pddl domain.pddl
+
+../limit.sh -t 1 -- ../macroff-clean p01.pddl domain.pddl
+
+../limit.sh -t 1 -- ../macroff-clean p18.pddl domain.pddl
+
+# increased verbosity
+../limit.sh -t 1 -v -- ../macroff-clean p01.pddl domain.pddl
+
+
+# debugging: do not remove temporary directory
+../limit.sh -t 1 -d -- ../macroff-clean  p03.pddl domain.pddl
+
+# what happens when there are no solution?
+../limit.sh -t 1 -v -- ../macroff-clean unsolvable.pddl domain.pddl
+
