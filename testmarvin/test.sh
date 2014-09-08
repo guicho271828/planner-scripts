7fa20e90363ddf656a4175632516b039bd1432a5
Masataro Asai <guicho2.71828@gmail.com>
2014-09-08 10:55:42 +0900

    added tests for marvin

--- testmarvin/test.sh
+++ testmarvin/test.sh
@@ -17,6 +17,17 @@
 ../clean.sh
 ../limit.sh -t 1 -- ../marvin-clean -v p01.pddl domain.pddl
 
+
+# debugging: do not remove temporary directory
+../clean.sh
+../limit.sh -t 1 -- ../marvin-clean -d p03.pddl domain.pddl
+
+# debugging: test marvin -l option
+../clean.sh
+../limit.sh -t 1 -- ../marvin-clean -d -o "-l" p03.pddl domain.pddl
+
+
+
 # what happens when there are no solution?
 ../clean.sh
 ../limit.sh -t 1 -- ../marvin-clean -v unsolvable.pddl domain.pddl
