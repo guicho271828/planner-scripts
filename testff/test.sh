5a544c7f0933c80d510e710dd881fe511a0d15f9
Masataro Asai <guicho2.71828@gmail.com>
2014-10-23 23:15:19 +0900

    fixed ff-clean tests

--- testff/test.sh
+++ testff/test.sh
@@ -5,19 +5,19 @@
 ## ff
 # this should finish normally (because it is easy)
 ../clean.sh
-../limit.sh macroff-clean p01.pddl domain.pddl
+../limit.sh ff-clean p01.pddl domain.pddl
 
-../limit.sh -t 1 -- macroff-clean p01.pddl domain.pddl
+../limit.sh -t 1 -- ff-clean p01.pddl domain.pddl
 
-../limit.sh -t 1 -- macroff-clean p18.pddl domain.pddl
+../limit.sh -t 1 -- ff-clean p18.pddl domain.pddl
 
 # increased verbosity
-../limit.sh -t 1 -v -- macroff-clean p01.pddl domain.pddl
+../limit.sh -t 1 -v -- ff-clean p01.pddl domain.pddl
 
 
 # debugging: do not remove temporary directory
-../limit.sh -t 1 -d -- macroff-clean  p03.pddl domain.pddl
+../limit.sh -t 1 -d -- ff-clean  p03.pddl domain.pddl
 
 # what happens when there are no solution?
-../limit.sh -t 1 -v -- macroff-clean unsolvable.pddl domain.pddl
+../limit.sh -t 1 -v -- ff-clean unsolvable.pddl domain.pddl
 
