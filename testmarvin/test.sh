3d59e2ad9296537366dfd87380331556d66c7cb7
Masataro Asai <guicho2.71828@gmail.com>
2015-03-29 11:42:51 +0900

    fixed testmarvin/test.sh

--- testmarvin/test.sh
+++ testmarvin/test.sh
@@ -5,22 +5,22 @@
 ## ff
 # this should finish normally (because it is easy)
 ../clean.sh
-../limit.sh -- marvin-clean p01.pddl domain.pddl
+../limit.sh -- marvin2-clean p01.pddl domain.pddl
 
-../limit.sh -t 1 -- marvin-clean p01.pddl domain.pddl
+../limit.sh -t 1 -- marvin2-clean p01.pddl domain.pddl
 
-../limit.sh -t 1 -- marvin-clean p18.pddl domain.pddl
+../limit.sh -t 1 -- marvin2-clean p18.pddl domain.pddl
 
 # increased verbosity
-../limit.sh -t 1 -v -- marvin-clean p01.pddl domain.pddl
+../limit.sh -t 1 -v -- marvin2-clean p01.pddl domain.pddl
 
 
 # debugging: do not remove temporary directory
-../limit.sh -t 1 -d -- marvin-clean  p03.pddl domain.pddl
+../limit.sh -t 1 -d -- marvin2-clean  p03.pddl domain.pddl
 
-# debugging: test marvin -l option
-../limit.sh -t 1 -d -o "-l" -- marvin-clean p03.pddl domain.pddl
+# debugging: test marvin2 -l option
+../limit.sh -t 1 -d -o "-l" -- marvin2-clean p03.pddl domain.pddl
 
 # what happens when there are no solution?
-../limit.sh -t 1 -v -- marvin-clean unsolvable.pddl domain.pddl
+../limit.sh -t 1 -v -- marvin2-clean unsolvable.pddl domain.pddl
 
