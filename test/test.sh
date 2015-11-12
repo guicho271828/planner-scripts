d9cab6f92b51770bb9b35de6c15ddb37870ed081
Masataro Asai <guicho2.71828@gmail.com>
2015-11-12 19:51:06 +0900

    added jasper and mercury

--- test/test.sh
+++ test/test.sh
@@ -17,8 +17,8 @@ test-non-ff (){
 
     section $1
     success ../limit.sh $1 p01.pddl domain.pddl
-
-    success ../limit.sh -t 1 -- $1 p01.pddl domain.pddl
+    success ../limit.sh -- $1 p02.pddl domain.pddl
+    success ../limit.sh -v -- $1 p03.pddl domain.pddl
 
     fail ../limit.sh -t 1 -- $1 p22.pddl domain.pddl
     fail test -e p22.negative
@@ -30,14 +30,14 @@ test-non-ff (){
     # success ../limit.sh -t 1 -d -- $1  p03.pddl domain.pddl
 
     # what happens when there are no solution?
-    fail ../limit.sh -t 1 -v -- $1 unsolvable.pddl domain.pddl
+    fail ../limit.sh -v -- $1 unsolvable.pddl domain.pddl
 
     success test -e unsolvable.negative
 }
 
 
 #  mff doesnt work at all
-for planner in ff lpg marvin1 marvin2 mpc mp m probe yahsp3
+for planner in  jasper mercury ff lpg marvin1 marvin2 mpc mp m probe yahsp3
 do
     test-non-ff $planner-clean
 done
