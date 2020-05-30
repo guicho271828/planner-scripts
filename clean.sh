a89f40eb33b0ac391d3508a2d20274635f6122f6
Masataro Asai <guicho2.71828@gmail.com>
2020-05-30 19:52:43 -0400

    bugfix: clean.sh should remove *.negative

--- clean.sh
+++ clean.sh
@@ -4,7 +4,7 @@ echo "##### START CLEANING #############################################"
 
 pushd ${1:-$(pwd)}
 pwd
-rm -fv *.sas *.sasp *.cost *.log *.err *.time *.plan* downward.tmp.*
+rm -fv *.sas *.sasp *.cost *.log *.err *.time *.plan* downward.tmp.* *.negative
 popd
 
 echo "##### END CLEANING #############################################"
